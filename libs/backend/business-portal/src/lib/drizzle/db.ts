077bc88b940e0c1d8578a0fa50f0ecdfcfbaebe7
GIANNIS\tsiro <tsiro1@hotmail.com>
2025-03-14 13:57:06 +0200

    db

--- libs/backend/business-portal/src/lib/drizzle/db.ts
+++ libs/backend/business-portal/src/lib/drizzle/db.ts
@@ -4,6 +4,7 @@ import * as schema from './schema';
 
 //const client = postgres(process.env['DATABASE_URL'] ?? '');
 const client = postgres(
-  'postgresql://postgres.smrapzkhjrhqwrserfao:YzfCwQunykpicAvE@aws-0-eu-central-1.pooler.supabase.com:6543/postgres'
+  //businessporta-db-password:
+  'postgresql://postgres.tikfxljuvjqcqeibbgey:[PASSWORD]@aws-0-eu-central-1.pooler.supabase.com:6543/postgres'
 );
 export const db = drizzle(client, { schema, logger: true });
